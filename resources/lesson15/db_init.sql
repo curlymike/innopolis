239d6715f5dc5f96679a2603750106248fd1519a
Mike <curlymike@gmail.com>
2019-06-13 14:48:37 +0300

    Начал ДЗ_13 - SQL скрипт готов

---
+++ resources/lesson15/db_init.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS "user" CASCADE;
+DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS role;
+DROP TYPE IF EXISTS roles;
+
+-- Таблица USER содержит поля id, name, birthday, login_ID, city, email, description
+-- Таблица ROLE содержит поля id, name (принимает значения Administration, Clients, Billing), description
+-- Таблица USER_ROLE содержит поля id, user_id, role_id
+
+CREATE TABLE "user"
+(
+  id               SERIAL PRIMARY KEY,
+  name             VARCHAR(255)            NOT NULL,
+  birthday         TIMESTAMP DEFAULT now() NOT NULL,
+  login_ID         VARCHAR(64)             NOT NULL,
+  city             VARCHAR(255)            NOT NULL,
+  email            VARCHAR(255)            NOT NULL,
+  description      TEXT                    NOT NULL
+);
+
+CREATE TYPE roles AS ENUM ('Administration', 'Clients', 'Billing');
+
+CREATE TABLE role
+(
+  id               SERIAL PRIMARY KEY,
+  name             roles,
+  description      TEXT                 NOT NULL
+);
+
+CREATE TABLE user_role
+(
+  id               SERIAL PRIMARY KEY,
+  user_id          INTEGER REFERENCES "user" (id) ON DELETE CASCADE,
+  role_id          INTEGER REFERENCES role (id) ON DELETE CASCADE,
+  UNIQUE (role_id, user_id)
+);
+
+/*
+CREATE TABLE user_role_alternative
+(
+  role_id          INTEGER,
+  user_id          INTEGER,
+  PRIMARY KEY(role_id, user_id)
+);
+*/
---
+++ src/part1/lesson15/Main.java
@@ -0,0 +1,16 @@
+package part1.lesson15;
+
+/***
+ * ДЗ_13
+ *
+ * 2) Через jdbc интерфейс сделать запись данных(INSERT) в таблицу
+ * a) Используя параметризированный запрос
+ * b) Используя batch процесс
+ *
+ */
+
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
